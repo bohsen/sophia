ef26b70d17249b7efb893b3eaa7d519889962237
bohsen <bohsen@gmail.com>
2023-12-11 23:36:14 +0100

    Added FileChooserDialog for choosing what directory to watch for changes

--- src/main/kotlin/Dialogs.kt
+++ src/main/kotlin/Dialogs.kt
@@ -1,53 +1,26 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.window.AwtWindow
 import androidx.compose.ui.window.FrameWindowScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.swing.Swing
 import org.apache.logging.log4j.kotlin.logger
-import java.awt.FileDialog
 import java.io.File
-import java.nio.file.Path
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileSystemView
 
 @Composable
-fun FrameWindowScope.FileDialog(
-    title: String,
-    isLoad: Boolean,
-    onResult: (result: Path?) -> Unit
-) = AwtWindow(
-    create = {
-        object : FileDialog(window, title, if (isLoad) LOAD else SAVE) {
-            override fun setVisible(value: Boolean) {
-                super.setVisible(value)
-                if (value) {
-                    if (file != null) {
-                        onResult(File(directory).resolve(file).toPath())
-                    } else {
-                        onResult(null)
-                    }
-                }
-            }
-        }.apply {
-            this.title = title
-        }
-    },
-    dispose = FileDialog::dispose
-)
-
-@Composable
 fun FrameWindowScope.FileChooserDialog(
     title: String,
-    onResult: (result: File) -> Unit
+    onResult: (result: File) -> Unit,
+    onCloseRequest: () -> Unit
 ) {
     val scope = rememberCoroutineScope()
     DisposableEffect(this) {
         val job = scope.launch(Dispatchers.Swing) {
 
-            logger.info("Opening filechooser...")
+            logger.info("Opening file-chooser...")
             JFileChooser(FileSystemView.getFileSystemView()).apply {
                 currentDirectory = File(System.getProperty("user.dir"))
                 dialogTitle = title
@@ -60,12 +33,12 @@ fun FrameWindowScope.FileChooserDialog(
                         logger.info { "Selected: $selectedFile" }
                         onResult(selectedFile)
                     }
-
                     else -> {
                         logger.info { "Cancel filechooser" }
                         onResult(File(""))
                     }
                 }
+                onCloseRequest()
             }
         }
 
